4b9ceb6c6acca943f36512d8462c232423886d91
Kimseonghyoek <cilo1223@naver.com>
2022-04-29 22:34:52 +0900

    add playerfunction

--- player.py
+++ player.py
@@ -15,7 +15,15 @@ class Player :
     def playerMoveUP(self):
         setting.playerY += 2.5
         print('up', setting.playerY)
-
+        pg.display.flip()
     def playerMoveDOWN(self):
         setting.playerY -= 2.5
         print('down', setting.playerY)
+
+    def playerMoveRight(self):
+        setting.playerX -= 2.5
+        print('right', setting.playerX)
+
+    def playerMoveRight(self):
+        setting.playerX -= 2.5
+        print('right', setting.playerX)
