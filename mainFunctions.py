6c7fc5f0f911ce2c0fcd2e84305931ad7412a276
Kimseonghyoek <cilo1223@naver.com>
2022-04-30 18:13:05 +0900

    add mainFunctions

--- mainFunctions.py
+++ mainFunctions.py
@@ -1,14 +1,13 @@
 import pygame as pg
 from setting import *
-from main import MAIN, screen
 
-class Function :
+screen = pg.display.set_mode(size)
 
-    # text를 써주는 기능
-    def drawText(self, text, size, color, x, y):
-        font = pg.font.Font(font_name, size)
-        text_suface = font.render(text, True, color)
-        text_rect = text_suface.get_rect()
-        text_rect.midtop = (x, y)
-        screen.blit(text_suface, text_rect)
-        pg.display.flip()
+# text를 써주는 기능
+def drawText(text, size, color, x, y):
+    font = pg.font.Font(font_name, size)
+    text_suface = font.render(text, True, color)
+    text_rect = text_suface.get_rect()
+    text_rect.midtop = (x, y)
+    screen.blit(text_suface, text_rect)
+    pg.display.flip()
