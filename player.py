4d573e3a8bb73a38df6f74697b6d35d5d2ebfa0f
Kimseonghyoek <cilo1223@naver.com>
2022-05-08 15:54:53 +0900

    add user photo

--- player.py
+++ player.py
@@ -2,27 +2,37 @@ import pygame as pg
 
 import setting
 from setting import *
+from main import screen
 
-class Player :
+class Peadsf :
+
+    walkCount = 0
 
     pg.init()
-    screen = pg.display.set_mode(size)
+    # screen = pg.display.set_mode(size)
+
+    rightWalk = [pg.image.load('user1.png'), pg.image.load('user2.png'), pg.image.load('user3.png')]
+    rightWalk = pg.transform.scale(rightWalk[0], (30, 55)), pg.transform.scale(rightWalk[1], (30, 55)), pg.transform.scale(rightWalk[2], (30, 55))
 
     # 플레이어 그리기
     def drawPlayer(self):
         self.screen.blit(PLAYERIMG, (playerX, playerY))
 
     def playerMoveUP(self):
-        setting.playerY -= 0.35
+        setting.playerY -= 0.25
         print('up', setting.playerY)
     def playerMoveDOWN(self):
-        setting.playerY += 0.35
+        setting.playerY += 0.25
         print('down', setting.playerY)
     def playerMoveLEFT(self):
-        setting.playerX -= 0.35
+        setting.playerX -= 0.25
         print('left', setting.playerX)
     def playerMoveRIGHT(self):
-        setting.playerX += 0.35
+        global walkCount
+        if walkCount >= 3 :
+            walkCount = 0
+        screen.blit(self.rightWalk[walkCount], (setting.playerX, setting.playerY))
+        setting.playerX += 0.25
         print('right', setting.playerX)
 
     # 플레이어 공격
